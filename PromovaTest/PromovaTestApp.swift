def94c868ef22a5a9ea5c199cbed0f363b6f829c
Yarosalv Kukhar <yaroslav.kukhar@globallogic.com>
2024-09-08 21:23:08 +0300

    Initial Commit

---
+++ PromovaTest/PromovaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestApp.swift
+//  PromovaTest
+//
+//  Created by Yaroslav Kukhar on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromovaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
